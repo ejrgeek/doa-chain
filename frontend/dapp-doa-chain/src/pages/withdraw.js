e40aeef29877048ef459d9a52857067c9aa75052
Erlon Jr. <erlon.dn.jr@gmail.com>
2024-12-05 17:50:36 -0300

    mend

--- frontend/dapp-doa-chain/src/pages/withdraw.js
+++ frontend/dapp-doa-chain/src/pages/withdraw.js
@@ -1,9 +1,11 @@
+import HeadNext from "@/components/Head";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 export default function Withdraw(){
     return (
         <>
+        <HeadNext title={"Withdraw"} />
         {/* HEADER */}
         <Header />
         
